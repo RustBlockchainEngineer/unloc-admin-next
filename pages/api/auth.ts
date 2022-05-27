7e0f2ff60d7ae6126e4ffcc75e5ed3ca492c3d5c
praskoson <40672437+praskoson@users.noreply.github.com>
2022-05-27 15:19:58 +0200

    Pretty

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,16 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { isWalletInWhitelist } from '../../functions/redis-queries'
 
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<{ isWhitelisted: boolean }>,
+  res: NextApiResponse<{ isWhitelisted: boolean }>
 ): Promise<void> => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     //TODO: interface needs to be implemented for req.body.address
-    const isWhitelisted = await isWalletInWhitelist('admins', req.body.address);
-    res.status(200).json({ isWhitelisted });
-  } else res.end(404);
-};
+    const isWhitelisted = await isWalletInWhitelist('admins', req.body.address)
+    res.status(200).json({ isWhitelisted })
+  } else res.end(404)
+}
 
-export default handler;
+export default handler
--- pages/api/count.ts
+++ pages/api/count.ts
@@ -1,16 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { getCollectionsWithCountOfNfts } from '../../functions/redis-queries'
 
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<Record<string, number> | string>,
+  res: NextApiResponse<Record<string, number> | string>
 ): Promise<void> => {
-  if (req.method === "GET") {
+  if (req.method === 'GET') {
     //TODO: interface needs to be implemented for req.body.address
-    const collections = await getCollectionsWithCountOfNfts();
-    res.status(200).json(collections);
-  } else res.end(404);
-};
+    const collections = await getCollectionsWithCountOfNfts()
+    res.status(200).json(collections)
+  } else res.end(404)
+}
 
-export default handler;
+export default handler
