bf635dbce641574ca6803361052d5d5719b2dab3
praskoson <40672437+praskoson@users.noreply.github.com>
2022-05-27 15:35:44 +0200

    Add whitelist api endpoint

---
+++ pages/api/whitelist.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { addUsersToWhitelist, isWalletInWhitelist } from '../../functions/redis-queries'
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<{ whitelistCount: number }>
+): Promise<void> => {
+  if (req.method === 'POST') {
+    //TODO: interface needs to be implemented for req.body.address
+    const addresses = req.body;
+    const count = await addUsersToWhitelist(addresses)
+    res.status(200).json({ whitelistCount: count })
+  } else res.end(404)
+}
+
+export default handler
