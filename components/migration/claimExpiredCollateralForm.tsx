6f5c0d9b7da944983c53e66025a45158af76b1c5
SolanaEngineer <topstack2021a@gmail.com>
2022-08-17 19:06:58 +0300

    add: claim collateral

--- components/migration/claimExpiredCollateralForm.tsx
+++ components/migration/claimExpiredCollateralForm.tsx
@@ -1,48 +1,43 @@
-import { TOKEN_PROGRAM_ID } from '../../node_modules/@solana/spl-token'
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { SystemProgram } from '@solana/web3.js'
-import { PublicKey, Transaction } from '@solana/web3.js'
-import { createClaimExpiredCollateralInstruction, offerBeet } from '@unloc-dev/unloc-loan-solita'
-import { SyntheticEvent, useCallback } from 'react'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { PublicKey} from '@solana/web3.js'
 import { Field, Form } from 'react-final-form'
-import { useStore } from '../../stores'
 import { InputAdapter } from './InputAdapter'
 import { claimExpiredCollateral } from '@unloc-dev/unloc-sdk'
 
 interface Values {
-  unlocStakingPid: string
-  unlocStakingPoolId: string
+  subOfferId: string
 }
 
 export const ClaimExpiredCollateralForm = () => {
-  const { connection } = useConnection()
-  const { programs } = useStore()
-  const { publicKey, sendTransaction } = useWallet()
-  const { loanGlobalState } = programs
-
-  const initialValues: Partial<Values> = {
-    unlocStakingPoolId: loanGlobalState?.unlocStakingPoolId.toBase58()
-  }
+  const { publicKey } = useWallet()
 
   const handleSubmit = async (values: Values) => {
     const superOwner = publicKey
     if (!superOwner) return
 
-    // await claimExpiredCollateral()
+    await claimExpiredCollateral(new PublicKey(values.subOfferId))
   }
 
   return (
     <Form<Values>
       onSubmit={handleSubmit}
-      render={({ handleSubmit, form }) => {
-        const handleFillCurrent = (e: SyntheticEvent) => {
-          e.preventDefault()
-          form.reset(initialValues)
-        }
-
+      render={({ handleSubmit}) => {
         return (
-          <form>
-            <div className='flex flex-col'>TODO</div>
+          <form onSubmit={handleSubmit} className='bg-unlocGray-500 p-4'>
+            <div className='flex flex-col'>
+              <Field<string>
+                name='subOfferId'
+                type='text'
+                label='Sub Offer Id'
+                component={InputAdapter}
+                required
+              ></Field>
+            </div>
+            <div className='mt-4 flex justify-end space-x-2'>
+              <button className='btn' type='submit'>
+                Submit
+              </button>
+            </div>
           </form>
         )
       }}
